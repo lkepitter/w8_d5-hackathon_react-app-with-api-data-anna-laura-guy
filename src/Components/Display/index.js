9c7f9a543591aad81b508470e70ce8ddacb7e4bb
lkepitter <lkepitter@hotmail.co.uk>
2022-01-14 14:58:05 +0000

    Added CORS proxy to fetch URL to avoid CORS errors.

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import {useState, useEffect} from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
-export function Display ({search}) {
-    const [character, setCharacter] = useState({})
+export function Display({ search }) {
+  const [character, setCharacter] = useState({});
 
-    
-    useEffect(()=>{
-        async function getCharacter(){
-            let response = await fetch(`https://the-one-api.dev/v2/character/?name=${search}`,{headers: {
-                accept: "application/json",
-                Authorization: "Bearer M_NvO75ukBdw8Ht5Bb-Z",
-              }})
-            let data = await response.json();
-            setCharacter(data.docs[0]);
-        };
-        getCharacter();
-                    
-        }, [search]
-    ); 
+  useEffect(() => {
+    async function getCharacter() {
+      let response = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://the-one-api.dev/v2/character/?name=${search}`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer M_NvO75ukBdw8Ht5Bb-Z",
+          },
+        }
+      );
+      let data = await response.json();
+      setCharacter(data.docs[0]);
+    }
+    getCharacter();
+  }, [search]);
 
-    return (
-        <div>
-           <img src= "https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg" alt="Gandalf scowling"></img> 
-           <div>
-            <p> name: {character.name}</p>
-            <p> hair: {character.hair}</p>
-            <p> </p>
-
-           </div>
-           
-
-        </div>
-    )
+  return (
+    <div>
+      <img
+        src="https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg"
+        alt="Gandalf scowling"
+      ></img>
+      <div>
+        <p> name: {character.name}</p>
+        <p> hair: {character.hair}</p>
+        <p> </p>
+      </div>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
