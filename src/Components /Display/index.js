a3525e852c8ecf0fbe2b3278c0a554bfe3679c49
“ilhamabdi” <ilhamabdi13@gmail.com>
2022-01-14 11:42:22 +0000

    Display Component Kinda

---
+++ src/Components /Display/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {useState, useEffect} from "react";
+
+export function Display ({props}) {
+    const [character, setCharacter] = useState({})
+
+    
+    useEffect(()=>{
+        async function getCharacter(){
+            let response = await fetch("https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfea0",{headers: {accept: "application/json"}})
+            let data = await response.json();
+            setCharacter(data.docs[0]);
+        };
+        getCharacter();
+                    
+        }, []
+    );
+
+    return (
+        <div>
+           <img src= "https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg" alt="Gandalf scowling"></img> 
+           <div>
+            <p> name: {character.name}</p>
+            <p> hair: {character.hair}</p>
+            <p> </p>
+
+           </div>
+           
+
+        </div>
+    )
+}
+
+export default Display
---
+++ src/plan.md
@@ -0,0 +1,16 @@
+Plan 
+1. Add folders and files for the components that we have
+Components that we have: Buttons( search, random, back and forth, save,), display 
+2. Start with Dsiplay, image, text containers, 
+3. Start on search button, 
+4. Create test for this button.
+5. Create Random button.
+6. Create test for Random button.
+7. Bonus: Navigation buttons,
+8. Bonus: Create test for this button.
+9. Bonus: Save button.
+10. Bonus: test for save button.
+
+
+
+
